e82ce733313f6fd9e4f2de28ef12a91f35e2adbe
Nisarg Mehta <nisargmehta23198@gmail.com>
2022-07-15 02:41:29 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const buttons = document.querySelectorAll(".drum");
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        let key = this.innerHTML;
+        playSound(key);        
+    })
+    
+}
+
+document.addEventListener("keydown", function(oEvent) {
+    let key = oEvent.key;
+    playSound(key);
+});
+
+function playSound(key) {
+    switch (key) {
+        case 'w':
+            var audio = new Audio('./sounds/crash.mp3');
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio('./sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio('./sounds/snare.mp3');
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio('./sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio('./sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio('./sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio('./sounds/tom-4.mp3');
+            audio.play();
+            break;
+        default:
+            break;
+    }
+}
